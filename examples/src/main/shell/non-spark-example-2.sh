e46f17dbcb3f5086599ccb51c19e19be98889fb2
Oleksandr Vayda <oleksandr.vayda@gmail.com>
2020-02-10 17:13:08 +0100

    spline #572 Cross-framework end-to-end lineage example

---
+++ examples/src/main/shell/non-spark-example-2.sh
@@ -0,0 +1,222 @@
+#!/bin/bash
+# ------------------------------------------------------------------------
+# Copyright 2020 ABSA Group Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ------------------------------------------------------------------------
+#
+
+SPLINE_GATEWAY_URL=http://localhost:8080
+
+_producer_url=$SPLINE_GATEWAY_URL/producer
+_consumer_url=$SPLINE_GATEWAY_URL/consumer
+
+_jan_job_search="Jan%27s%20beer%20job"
+
+_jan_job_exec_id=$(eval echo "$(curl -s $_consumer_url/execution-events?searchTerm="$_jan_job_search" | jq ".items[0].executionPlanId")")
+_jan_job_timestamp=$(curl -s $_consumer_url/execution-events?searchTerm="$_jan_job_search" | jq ".items[0].timestamp")
+_beer_source=$(curl -s $_consumer_url/lineage-detailed?execId="$_jan_job_exec_id" | jq ".executionPlan.inputs[].source" | grep beer)
+_new_job_timestamp=$((_jan_job_timestamp - 1000))
+
+# Prepare execution plan
+_exec_plan_json=$(
+  cat <<END
+{
+  "id": "$(uuidgen)",
+  "agentInfo": {
+    "name": "Bash",
+    "version": "$BASH_VERSION"
+  },
+  "systemInfo": {
+    "name": "Dummy Example",
+    "version": "0.0.0"
+  },
+  "extraInfo": {
+    "appName": "Beer Consumption Stats"
+  },
+  "operations": {
+    "write": {
+      "id": 0,
+      "outputSource": $_beer_source,
+      "append": false,
+      "childIds": [1],
+      "extra": {
+        "name": "Write"
+      }
+    },
+    "other": [
+      {
+        "id": 1,
+        "childIds": [2, 19],
+        "extra": {
+          "name": "Join"
+        }
+      },
+      {
+        "id": 2,
+        "childIds": [3, 18],
+        "extra": {
+          "name": "Join"
+        }
+      },
+      {
+        "id": 3,
+        "childIds": [4, 17],
+        "extra": {
+          "name": "Join"
+        }
+      },
+      {
+        "id": 4,
+        "childIds": [5, 16],
+        "extra": {
+          "name": "Join"
+        }
+      },
+      {
+        "id": 5,
+        "childIds": [6, 15],
+        "extra": {
+          "name": "Join"
+        }
+      },
+      {
+        "id": 6,
+        "childIds": [7, 14],
+        "extra": {
+          "name": "Join"
+        }
+      },
+      {
+        "id": 7,
+        "childIds": [8, 13],
+        "extra": {
+          "name": "Join"
+        }
+      },
+      {
+        "id": 8,
+        "childIds": [9, 12],
+        "extra": {
+          "name": "Join"
+        }
+      },
+      {
+        "id": 9,
+        "childIds": [10, 11],
+        "extra": {
+          "name": "Join"
+        }
+      }
+    ],
+    "reads": [
+      {
+        "id": 10,
+        "inputSources": ["hdfs://.../country_codes"],
+        "extra": {
+          "name": "Read - Country Codes"
+        }
+      },
+      {
+        "id": 11,
+        "inputSources": ["hdfs://.../beer_consum_2003.csv"],
+        "extra": {
+          "name": "Get stats (year 2003)"
+        }
+      },
+      {
+        "id": 12,
+        "inputSources": ["hdfs://.../beer_consum_2004.csv"],
+        "extra": {
+          "name": "Get stats (year 2004)"
+        }
+      },
+      {
+        "id": 13,
+        "inputSources": ["hdfs://.../beer_consum_2005.csv"],
+        "extra": {
+          "name": "Get stats (year 2005)"
+        }
+      },
+      {
+        "id": 14,
+        "inputSources": ["hdfs://.../beer_consum_2006.csv"],
+        "extra": {
+          "name": "Get stats (year 2006)"
+        }
+      },
+      {
+        "id": 15,
+        "inputSources": ["hdfs://.../beer_consum_2007.csv"],
+        "extra": {
+          "name": "Get stats (year 2007)"
+        }
+      },
+      {
+        "id": 16,
+        "inputSources": ["hdfs://.../beer_consum_2008.csv"],
+        "extra": {
+          "name": "Get stats (year 2008)"
+        }
+      },
+      {
+        "id": 17,
+        "inputSources": ["hdfs://.../beer_consum_2009.csv"],
+        "extra": {
+          "name": "Get stats (year 2009)"
+        }
+      },
+      {
+        "id": 18,
+        "inputSources": ["hdfs://.../beer_consum_2010.csv"],
+        "extra": {
+          "name": "Get stats (year 2010)"
+        }
+      },
+      {
+        "id": 19,
+        "inputSources": ["hdfs://.../beer_consum_2011.csv"],
+        "extra": {
+          "name": "Get stats (year 2011)"
+        }
+      }
+    ]
+  }
+}
+END
+)
+
+#echo $_exec_plan_json
+
+
+# POST execution plan
+_exec_plan_id=$(curl -s -d "$_exec_plan_json" -H 'Content-Type: application/json' $_producer_url/execution-plans)
+
+# Prepare execution event
+_exec_event_json=$(
+  cat <<END
+[{
+  "planId": $_exec_plan_id,
+  "timestamp": $_new_job_timestamp
+}]
+END
+)
+
+# POST execution event
+curl -d "$_exec_event_json" -H 'Content-Type: application/json' $_producer_url/execution-events
+
+echo "
+  Non-Spark Lineage recorded:
+    Execution Plan ID: $_exec_plan_id
+    Execution Timestamp: $_new_job_timestamp
+"
