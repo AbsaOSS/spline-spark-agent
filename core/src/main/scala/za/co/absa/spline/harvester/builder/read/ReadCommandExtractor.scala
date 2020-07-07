4160ef36b58c6a8e4ea3e19ebfaaab9619203cd9
Steve Chaloner <steve.chaloner@kbc.be>
2020-07-07 09:24:05 +0200

    Pluggable handler for arbitrary relations

--- core/src/main/scala/za/co/absa/spline/harvester/builder/read/ReadCommandExtractor.scala
+++ core/src/main/scala/za/co/absa/spline/harvester/builder/read/ReadCommandExtractor.scala
@@ -43,7 +43,9 @@ import scala.collection.JavaConverters._
 import scala.util.Try
 
 
-class ReadCommandExtractor(pathQualifier: PathQualifier, session: SparkSession) {
+class ReadCommandExtractor(pathQualifier: PathQualifier,
+                           session: SparkSession,
+                           relationHandler: ReadRelationHandler = NoOpReadRelationHandler()) {
   def asReadCommand(operation: LogicalPlan): Option[ReadCommand] =
     condOpt(operation) {
       case lr: LogicalRelation => lr.relation match {
@@ -126,7 +128,12 @@ class ReadCommandExtractor(pathQualifier: PathQualifier, session: SparkSession) 
           ReadCommand(SourceIdentifier.forCobrix(sourceDir), operation)
 
         case br: BaseRelation =>
-          sys.error(s"Relation is not supported: $br")
+          if (relationHandler.isApplicable(br)) {
+            relationHandler(br,
+                            operation)
+          } else {
+            sys.error(s"Relation is not supported: $br")
+          }
       }
 
       case htr: HiveTableRelation =>
---
+++ core/src/test/scala/za/co/absa/spline/harvester/builder/read/ReadCommandExtractorSpec.scala
@@ -0,0 +1,65 @@
+package za.co.absa.spline.harvester.builder.read
+
+import org.apache.hadoop.conf.Configuration
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql.catalyst.expressions.Attribute
+import org.apache.spark.sql.catalyst.plans.logical.{LeafNode, LogicalPlan, Statistics}
+import org.apache.spark.sql.execution.datasources.LogicalRelation
+import org.apache.spark.sql.sources.BaseRelation
+import org.apache.spark.sql.types.StructType
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.must.Matchers
+import org.scalatestplus.mockito.MockitoSugar
+import za.co.absa.commons.spark.SparkTestBase
+import za.co.absa.spline.harvester.builder.SourceIdentifier
+import za.co.absa.spline.harvester.qualifier.HDFSPathQualifier
+
+import scala.util.{Failure, Try}
+
+class ReadCommandExtractorSpec extends AnyFlatSpec
+                                       with Matchers
+                                       with SparkTestBase
+                                       with MockitoSugar {
+
+  private val conf = new Configuration()
+  private val pathQualifier = new HDFSPathQualifier(conf)
+
+  it must "defer to the relation handler if there is a compatible one" in {
+    val logicalPlan: TestLogicalPlan = new TestLogicalPlan(TestRelation(spark.sqlContext))
+    object TestRelationHandler extends ReadRelationHandler {
+      override def isApplicable(relation: BaseRelation): Boolean = relation.isInstanceOf[TestRelation]
+
+      override def apply(relation: BaseRelation,
+                         logicalPlan: LogicalPlan): ReadCommand = ReadCommand(SourceIdentifier(Some("test")),
+                                                                              logicalPlan)
+    }
+
+    val result: Option[ReadCommand] = new ReadCommandExtractor(pathQualifier,
+                                                               spark,
+                                                               TestRelationHandler)
+      .asReadCommand(logicalPlan)
+    result.isDefined mustBe true
+    result.map(rc => rc.sourceIdentifier).flatMap(si => si.format).getOrElse("fail") mustBe "test"
+  }
+
+  it must "return a None if there is no way to handle the relation" in {
+    val relationNotHandled: Boolean = Try(new ReadCommandExtractor(pathQualifier,
+                                                                   spark)
+                                            .asReadCommand(new TestLogicalPlan(TestRelation(spark.sqlContext)))) match {
+      case _: Failure[_] => true
+      case _ => false
+    }
+    relationNotHandled mustBe true
+  }
+}
+
+private case class TestRelation(sqlCxt: SQLContext) extends BaseRelation {
+  override def sqlContext: SQLContext = sqlCxt
+
+  override def schema: StructType = StructType(Seq.empty)
+}
+
+private class TestLogicalPlan(delegate: BaseRelation) extends LogicalRelation(delegate,
+                                                                              Seq.empty,
+                                                                              None,
+                                                                              false)
