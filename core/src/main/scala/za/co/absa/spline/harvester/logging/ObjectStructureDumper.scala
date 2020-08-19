096514946851f26345cdf80290024c55427bf0f2
Adam Cervenka <adam.cervenka@absa.africa>
2020-08-19 17:24:53 +0200

    #114 log unrecognized operation details (#122)

---
+++ core/src/main/scala/za/co/absa/spline/harvester/logging/ObjectStructureDumper.scala
@@ -0,0 +1,111 @@
+/*
+ * Copyright 2020 ABSA Group Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package za.co.absa.spline.harvester.logging
+
+import java.lang.reflect.Modifier
+
+import za.co.absa.commons.reflect.ReflectionUtils.extractFieldValue
+
+import scala.annotation.tailrec
+
+object ObjectStructureDumper {
+
+  def dump(obj: Any): String = {
+    val value = obj.asInstanceOf[AnyRef]
+
+    val initialValue = ObjectBox(value, "operation", value.getClass.getName, 0)
+
+    val info = objectToStringRec(List(initialValue), Set.empty[InstanceEqualityBox], "")
+    s"Data for instance of ${value.getClass}\n$info"
+  }
+
+  private case class InstanceEqualityBox(obj: AnyRef) {
+    override def equals(otherObj: Any): Boolean = otherObj match {
+      case InstanceEqualityBox(inner) => inner eq obj
+      case _ => throw new UnsupportedOperationException()
+    }
+
+    override def hashCode(): Int = obj.getClass.getName.hashCode
+  }
+
+  private type VisitedSet = Set[InstanceEqualityBox]
+
+  private def addToVisited(visited: VisitedSet, obj: AnyRef): VisitedSet = visited + InstanceEqualityBox(obj)
+
+  private def wasVisited(visited: VisitedSet, obj: AnyRef): Boolean = visited(InstanceEqualityBox(obj))
+
+  private case class ObjectBox(value: AnyRef, fieldName: String, fieldType: String, depth: Int)
+
+  @tailrec
+  private final def objectToStringRec(stack: List[ObjectBox], visited: VisitedSet, result: String): String = stack match {
+    case Nil => result
+    case head :: tail => {
+      val value = head.value
+      val depth = head.depth
+
+      val (fieldsDetails, newStack, newVisited) = value match {
+        case null => ("= null", tail, visited)
+        case v if isReadyForPrint(v) => (s"= $v", tail, visited)
+        case v if wasVisited(visited, v) => ("! Object was already logged", tail, visited)
+        case None => ("= None", tail, visited)
+        case Some(x) => {
+          val newVal = ObjectBox(x.asInstanceOf[AnyRef], "x", x.getClass.getName, depth + 1)
+          ("Some", newVal :: tail, addToVisited(visited, value))
+        }
+        case _ => {
+          val newFields = value.getClass.getDeclaredFields
+            .filter(f => !Set("child", "session")(f.getName))
+            .filter(f => !Modifier.isStatic(f.getModifiers))
+            .map { f =>
+              val subValue = extractFieldValue[AnyRef](value, f.getName)
+              ObjectBox(subValue, f.getName, f.getType.getName, depth + 1)
+            }.toList
+
+          ("", newFields ::: tail, addToVisited(visited, value))
+        }
+      }
+
+      val indent = " " * depth * 2
+      val line = s"$indent${head.fieldName}: ${head.fieldType} $fieldsDetails\n"
+
+      objectToStringRec(newStack, newVisited, result + line)
+    }
+  }
+
+  private def isReadyForPrint(value: AnyRef): Boolean = {
+    isPrimitiveLike(value) ||
+      Set("String")(value.getClass.getSimpleName) ||
+      value.isInstanceOf[Traversable[_]] ||
+      value.isInstanceOf[Enum[_]]
+  }
+
+  private def isPrimitiveLike(value: Any): Boolean = {
+    val boxes = Set(
+      classOf[java.lang.Boolean],
+      classOf[java.lang.Byte],
+      classOf[java.lang.Character],
+      classOf[java.lang.Float],
+      classOf[java.lang.Integer],
+      classOf[java.lang.Long]
+    ).map(_.getName)
+
+    val isPrimitive = (value: Any) => value.getClass.isPrimitive
+    val isBoxedPrimitive = (value: Any) => boxes(value.getClass.getName)
+
+    isPrimitive(value) || isBoxedPrimitive(value)
+  }
+}
