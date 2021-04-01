bb41c091b0fb2606b4ea2303661c5a1e45b19dd5
Alex Vayda <oleksandr.vayda@gmail.com>
2021-04-01 02:14:37 +0200

    Update build-all.sh
    Do a proper clean up after the cross build.
    Extract a global list of supported Scala binary versions.

--- build-all.sh
+++ build-all.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # ------------------------------------------------------------------------
 # Copyright 2020 ABSA Group Limited
 #
@@ -18,21 +18,30 @@
 # THIS SCRIPT IS INTENDED FOR LOCAL DEV USAGE ONLY
 #
 
+SCALA_VERSIONS=(2.11 2.12)
 BASE_DIR=$(dirname "$0")
 
-cross_build() {
-  SCALA_VER=$1
-  echo "Building with Scala $SCALA_VER"
-  find $BASE_DIR/target/* -type d -exec rm -rf {} \;
-  mvn scala-cross-build:change-version -Pscala-$SCALA_VER
-  mvn install -Pscala-$SCALA_VER
+log() {
+  echo "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░"
+  echo "                           $1                                                  "
+  echo "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░"
 }
 
-# ------------------------------------------------
+cross_build() {
+  bin_ver=$1
+  log "Building with Scala $bin_ver"
+  find "$BASE_DIR"/target/* -type d -exec rm -rf {} \; 2> /dev/null
 
-mvn clean
+  mvn scala-cross-build:change-version -Pscala-$bin_ver
+  mvn install -Pscala-$bin_ver
+}
 
-cross_build 2.11
-cross_build 2.12
+# -------------------------------------------------------------------------------
 
-mvn scala-cross-build:restore-version
+for v in "${SCALA_VERSIONS[@]}"; do
+  cross_build "$v"
+done
+
+log "Restoring POM-files"
+scala_profiles=$(for v in ${SCALA_VERSIONS[*]}; do echo "-Pscala-$v"; done)
+mvn scala-cross-build:restore-version $scala_profiles
