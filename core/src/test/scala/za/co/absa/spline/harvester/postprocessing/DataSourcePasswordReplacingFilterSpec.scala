6a6acf47469ea5529e5134b05cc791c810a755c2
Alex Vayda <oleksandr.vayda@gmail.com>
2021-05-12 11:38:38 +0200

    bugfix/spline-69 hide more passwords (#220)
    * spline-spark-agent #69 DataSourcePasswordReplacingFilter to recursively scan operation params.
    
    * spline-spark-agent #69 DataSourcePasswordReplacingFilter to recursively scan operation params.
    
    * spline-spark-agent #69 addressing PR comments: remove unnecessary lookahead from the regexp
    
    * spline-spark-agent #69 addressing PR comments: rename method parameter

--- core/src/test/scala/za/co/absa/spline/harvester/postprocessing/DataSourcePasswordReplacingFilterSpec.scala
+++ core/src/test/scala/za/co/absa/spline/harvester/postprocessing/DataSourcePasswordReplacingFilterSpec.scala
@@ -21,51 +21,97 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import org.scalatestplus.mockito.MockitoSugar
 import za.co.absa.spline.harvester.HarvestingContext
-import za.co.absa.spline.producer.model.v1_1.WriteOperation
+import za.co.absa.spline.producer.model.v1_1.{ReadOperation, WriteOperation}
 
 class DataSourcePasswordReplacingFilterSpec extends AnyFlatSpec with Matchers with MockitoSugar {
 
   private val defaultProperties =
     new PropertiesConfiguration(getClass.getResource("/spline.default.properties"))
-      .subset("spline.postProcessingFilter.dsUriPasswordReplace")
+      .subset("spline.postProcessingFilter.dsPasswordReplace")
 
-  private val ctxMock = mock[HarvestingContext]
+  private val dummyContext = mock[HarvestingContext]
 
   it should "mask secret in URL query parameter as `password=*****`" in { //NOSONAR
-    val wop = WriteOperation(
-      "" +
-        "jdbc:sqlserver://database.windows.net:1433" +
-        ";user=sample" +
-        ";password=12345" + //NOSONAR
-        ";password=" + //NOSONAR -- empty password is also a password
-        ";encrypt=true" +
-        ";trustServerCertificate=false" +
-        ";hostNameInCertificate=*.database.windows.net" +
-        ";loginTimeout=30:",
-      append = false, "", None, Nil, None, None)
-
-    val filter = new DataSourcePasswordReplacingFilter(defaultProperties)
-    val filteredOp = filter.processWriteOperation(wop, ctxMock)
-
-    filteredOp.outputSource shouldEqual "" +
+    val originalURL = "" +
       "jdbc:sqlserver://database.windows.net:1433" +
       ";user=sample" +
-      ";password=*****" + //NOSONAR <-- PASSWORD SHOULD BE SANITIZED
-      ";password=*****" + //NOSONAR <-- PASSWORD SHOULD BE SANITIZED
+      ";password=12345" + //NOSONAR
+      ";PASSPHRASE=" + //NOSONAR -- empty password is also a password
       ";encrypt=true" +
       ";trustServerCertificate=false" +
       ";hostNameInCertificate=*.database.windows.net" +
       ";loginTimeout=30:"
-  }
 
-  it should "mask secret in URL userinfo as `user:*****@host`" in {
-    val wop1 = WriteOperation("mongodb://bob:super_secret@mongodb.host.example.org:27017?authSource=admin", append = false, "", None, Nil, None, None) //NOSONAR
-    val wop2 = WriteOperation("mongodb://bob:@mongodb.host.example.org:27017?authSource=admin", append = false, "", None, Nil, None, None)
+    val sanitizedURL = "" +
+      "jdbc:sqlserver://database.windows.net:1433" +
+      ";user=sample" +
+      ";password=*****" + //NOSONAR <-- PASSWORD SHOULD BE SANITIZED
+      ";PASSPHRASE=*****" + //NOSONAR <-- PASSWORD SHOULD BE SANITIZED
+      ";encrypt=true" +
+      ";trustServerCertificate=false" +
+      ";hostNameInCertificate=*.database.windows.net" +
+      ";loginTimeout=30:"
+
+    val rop = ReadOperation(Seq(originalURL), "", None, None, None, None)
+    val wop = WriteOperation(originalURL, append = false, "", None, Nil, None, None)
 
     val filter = new DataSourcePasswordReplacingFilter(defaultProperties)
 
-    filter.processWriteOperation(wop1, ctxMock).outputSource shouldEqual "mongodb://bob:*****@mongodb.host.example.org:27017?authSource=admin" //NOSONAR
-    filter.processWriteOperation(wop2, ctxMock).outputSource shouldEqual "mongodb://bob:*****@mongodb.host.example.org:27017?authSource=admin" //NOSONAR
+    filter.processReadOperation(rop, dummyContext).inputSources shouldEqual Seq(sanitizedURL)
+    filter.processWriteOperation(wop, dummyContext).outputSource shouldEqual sanitizedURL
+  }
+
+  it should "mask secret in URL userinfo as `user:*****@host`" in {
+    val rop = ReadOperation(Seq("mongodb://bob:super_secret@mongodb.host.example.org:27017?authSource=admin"), "", None, None, None, None) //NOSONAR
+    val wop = WriteOperation("mongodb://bob:@mongodb.host.example.org:27017?authSource=admin", append = false, "", None, Nil, None, None) //NOSONAR
+
+    val filter = new DataSourcePasswordReplacingFilter(defaultProperties)
+
+    filter.processReadOperation(rop, dummyContext).inputSources shouldEqual Seq("mongodb://bob:*****@mongodb.host.example.org:27017?authSource=admin") //NOSONAR
+    filter.processWriteOperation(wop, dummyContext).outputSource shouldEqual "mongodb://bob:*****@mongodb.host.example.org:27017?authSource=admin" //NOSONAR
+  }
+
+  it should "mask secrets in 'params'" in {
+    val originalParams = Map(
+      "a" -> 42,
+      "b" -> Seq(null),
+      "c" -> None,
+      "m" -> Map(
+        "url" -> "jdbc:postgresql://bob:secret@someHost:somePort/someDB",
+        "dbtable" -> "someTable",
+        "user" -> "someUser",
+        "PASSPHRASE" -> "somePassword" // NOSONAR
+      ),
+      "url" -> "jdbc:postgresql://bob:secret@someHost:somePort/someDB",
+      "dbtable" -> "someTable",
+      "user" -> "someUser",
+      "password" -> "somePassword" // NOSONAR
+    )
+
+    val sanitizedParams = Map(
+      "a" -> 42,
+      "b" -> Seq(null),
+      "c" -> None,
+      "url" -> "jdbc:postgresql://bob:*****@someHost:somePort/someDB",
+      "m" -> Map(
+        "url" -> "jdbc:postgresql://bob:*****@someHost:somePort/someDB",
+        "dbtable" -> "someTable",
+        "user" -> "someUser",
+        "PASSPHRASE" -> "*****" // NOSONAR
+      ),
+      "url" -> "jdbc:postgresql://bob:*****@someHost:somePort/someDB",
+      "dbtable" -> "someTable",
+      "user" -> "someUser",
+      "password" -> "*****" // NOSONAR
+    )
+
+    val rop = ReadOperation(Nil, "", None, None, params = Some(originalParams), None)
+    val wop = WriteOperation("", append = false, "", None, Nil, params = Some(originalParams), None)
+
+    val filter = new DataSourcePasswordReplacingFilter(defaultProperties)
+
+    filter.processReadOperation(rop, dummyContext).params shouldEqual Some(sanitizedParams)
+    filter.processWriteOperation(wop, dummyContext).params shouldEqual Some(sanitizedParams)
   }
 
 }
