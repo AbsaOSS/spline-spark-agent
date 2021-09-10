5879e6179b8529c9ab3b6b1faf87ea48fbe1fcd7
Oleksandr Vayda <oleksandr.vayda@gmail.com>
2021-09-10 18:25:17 +0200

    issue #277 Fix BigQueryPlugin + example

--- core/src/main/scala/za/co/absa/spline/harvester/plugin/embedded/BigQueryPlugin.scala
+++ core/src/main/scala/za/co/absa/spline/harvester/plugin/embedded/BigQueryPlugin.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 ABSA Group Limited
+ * Copyright 2021 ABSA Group Limited
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,16 +16,24 @@
 
 package za.co.absa.spline.harvester.plugin.embedded
 
-import com.google.cloud.bigquery.TableId
-import javax.annotation.Priority
+import io.github.classgraph.ClassGraph
+import org.apache.hadoop.conf.Configuration
+import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.execution.datasources.{LogicalRelation, SaveIntoDataSourceCommand}
+import org.apache.spark.sql.internal.SQLConf
 import org.apache.spark.sql.sources.BaseRelation
+import org.apache.spark.sql.types.StructType
 import za.co.absa.commons.reflect.ReflectionUtils.extractFieldValue
 import za.co.absa.commons.reflect.extractors.SafeTypeMatchingExtractor
 import za.co.absa.spline.harvester.builder.SourceIdentifier
 import za.co.absa.spline.harvester.plugin.Plugin.{Precedence, ReadNodeInfo, WriteNodeInfo}
-import za.co.absa.spline.harvester.plugin.embedded.BigQueryPlugin.{`_: BigQuerySourceRelation`, _}
+import za.co.absa.spline.harvester.plugin.embedded.BigQueryPlugin._
 import za.co.absa.spline.harvester.plugin.{BaseRelationProcessing, Plugin, RelationProviderProcessing}
+
+import java.util.Optional
+import javax.annotation.Priority
+import scala.collection.JavaConverters._
+import scala.language.reflectiveCalls
 
 
 @Priority(Precedence.Normal)
@@ -37,32 +45,129 @@ class BigQueryPlugin
   import za.co.absa.commons.ExtractorImplicits._
 
   override def baseRelationProcessor: PartialFunction[(BaseRelation, LogicalRelation), ReadNodeInfo] = {
-    case (`_: BigQuerySourceRelation`(bq), _) =>
+    case (`_: DirectBigQueryRelation`(bq), _) =>
 
-      val tableId = extractFieldValue[TableId](bq, "tableId")
-      val project = tableId.getProject
-      val schema = tableId.getDataset
-      val table = tableId.getTable
+      val tableId = extractFieldValue[AnyRef](bq, "tableId")
+      val project = extractFieldValue[String](tableId, "project")
+      val dataset = extractFieldValue[String](tableId, "dataset")
+      val table = extractFieldValue[String](tableId, "table")
 
-      (asSourceId(project, schema, table), Map.empty)
+      (asSourceId(project, dataset, table), Map.empty)
 
   }
 
   override def relationProviderProcessor: PartialFunction[(AnyRef, SaveIntoDataSourceCommand), WriteNodeInfo] = {
-    case (rp, cmd) if rp == "com.google.cloud.spark.bigquery" || BigQuerySourceExtractor.matches(rp) =>
-      val schema = cmd.options("schema")
-      val table = cmd.options("table")
-      val project = cmd.options("project")
-      (asSourceId(project, schema, table), cmd.mode, cmd.query, cmd.options)
+    case (rp, cmd) if rp == "com.google.cloud.spark.bigquery" || BigQueryRelationProviderExtractor.matches(rp) =>
+
+      def cmdOptionKeysStr = cmd.options.keys.mkString(", ")
+
+      def bigQueryConfig = {
+        val Some(spark) = SparkSession.getActiveSession
+        SparkBigQueryConfig.from(
+          ImmutableMap.copyOf(cmd.options.asJava),
+          ImmutableMap.copyOf(spark.conf.getAll.asJava),
+          spark.sparkContext.hadoopConfiguration,
+          0,
+          spark.sessionState.conf,
+          spark.version,
+          Optional.empty()
+        )
+      }
+
+      val tableIdStr =
+        cmd.options.get("path")
+          .orElse(cmd.options.get("table"))
+          .getOrElse(sys.error(s"Cannot find table info in the command. Options available: $cmdOptionKeysStr"))
+
+      val tableId = TableId.parseTableId(tableIdStr)
+
+      val table =
+        Option(tableId.getTable)
+          .getOrElse(sys.error("Table name cannot be `null`"))
+
+      val dataset =
+        Option(tableId.getDataset)
+          .orElse(cmd.options.get("dataset"))
+          .getOrElse(sys.error(s"Cannot find dataset info in the table ID ($tableIdStr) or in the command. Options available: $cmdOptionKeysStr"))
+
+      val project =
+        Option(tableId.getProject)
+          .orElse(cmd.options.get("project"))
+          .orElse(Option(bigQueryConfig.getParentProjectId))
+          .getOrElse(sys.error(s"Cannot find project info in the table ID ($tableIdStr) or in the command. Options available: $cmdOptionKeysStr"))
+
+      (asSourceId(project, dataset, table), cmd.mode, cmd.query, cmd.options)
   }
 }
 
 object BigQueryPlugin {
 
-  object `_: BigQuerySourceRelation` extends SafeTypeMatchingExtractor[AnyRef]("com.google.cloud.spark.bigquery.direct.DirectBigQueryRelation")
+  //noinspection SameParameterValue
+  private def findPossiblyShadedClass(packagePrefix: String, classNameSuffix: String): Class[_] = {
+    new ClassGraph()
+      .acceptPackages(packagePrefix)
+      .scan()
+      .getAllClasses
+      .asScala
+      .filter(_.getName.endsWith(classNameSuffix))
+      .map(_.loadClass())
+      .headOption
+      .getOrElse(sys.error(s"Cannot find class `...$classNameSuffix` in package `$packagePrefix`"))
+  }
 
-  private object BigQuerySourceExtractor extends SafeTypeMatchingExtractor(classOf[com.google.cloud.spark.bigquery.DefaultSource])
+  //
+  // We cannot use the following static types here due to possible shading, so we'll mimic them.
+  //
 
-  private def asSourceId(project: String, schema: String, table: String) = SourceIdentifier(Some("bigquery"), s"bigquery:$project: $schema :$table")
+  object TableId {
+    type TableId = {
+      def getProject: String
+      def getDataset: String
+      def getTable: String
+    }
+    private val clazz = findPossiblyShadedClass("com.google.cloud", "com.google.cloud.bigquery.connector.common.BigQueryUtil")
+    val parseTableId: String => TableId =
+      clazz
+        .getMethod("parseTableId", classOf[String])
+        .invoke(clazz, _)
+        .asInstanceOf[TableId]
+  }
+
+  object ImmutableMap {
+    type ImmutableMap = AnyRef
+    private val clazz = findPossiblyShadedClass("com.google", "com.google.common.collect.ImmutableMap")
+    val copyOf: AnyRef => ImmutableMap =
+      clazz
+        .getMethod("copyOf", classOf[java.util.Map[_, _]])
+        .invoke(clazz, _)
+        .asInstanceOf[ImmutableMap]
+  }
+
+  object SparkBigQueryConfig {
+    type SparkBigQueryConfig = {
+      def getParentProjectId: String
+    }
+    private val clazz = findPossiblyShadedClass("com.google.cloud", "com.google.cloud.spark.bigquery.SparkBigQueryConfig")
+    private val immutableMapClass = findPossiblyShadedClass("com.google", "com.google.common.collect.ImmutableMap")
+    val from: (ImmutableMap.ImmutableMap, ImmutableMap.ImmutableMap, Configuration, Integer, SQLConf, String, Optional[StructType]) => SparkBigQueryConfig =
+      clazz
+        .getMethod("from",
+          classOf[java.util.Map[String, String]],
+          immutableMapClass,
+          classOf[Configuration],
+          classOf[Int],
+          classOf[SQLConf],
+          classOf[String],
+          classOf[Optional[StructType]]
+        )
+        .invoke(clazz, _, _, _, _, _, _, _)
+        .asInstanceOf[SparkBigQueryConfig]
+  }
+
+  object `_: DirectBigQueryRelation` extends SafeTypeMatchingExtractor[AnyRef]("com.google.cloud.spark.bigquery.direct.DirectBigQueryRelation")
+
+  private object BigQueryRelationProviderExtractor extends SafeTypeMatchingExtractor("com.google.cloud.spark.bigquery.BigQueryRelationProvider")
+
+  private def asSourceId(project: String, dataset: String, table: String) = SourceIdentifier(Some("bigquery"), s"bigquery://$project:$dataset.$table")
 
 }
