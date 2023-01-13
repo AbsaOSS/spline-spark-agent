390309aa27b99b2a21af60745167263e8a20a210
Adam Cervenka <adam.cervenka@absa.africa>
2023-01-13 14:46:03 +0100

    spark agent #571 fix dto mapper (#572) (backport from develop)
    * spark agent #571 fix dto mapper

--- core/src/main/scala/za/co/absa/spline/harvester/dispatcher/modelmapper/ModelMapperV12.scala
+++ core/src/main/scala/za/co/absa/spline/harvester/dispatcher/modelmapper/ModelMapperV12.scala
@@ -16,48 +16,111 @@
 
 package za.co.absa.spline.harvester.dispatcher.modelmapper
 
-import io.bfil.automapper._
+import za.co.absa.commons.SplineTraversableExtension._
 import za.co.absa.spline.producer.dto.v1_2
 import za.co.absa.spline.producer.model._
 
 import scala.language.implicitConversions
 
-/**
- * This class is using bfil automapper, unfortunately some parts of the mappings must be explicitly defined to
- * work around this bug: https://github.com/bfil/scala-automapper/issues/16
- */
 object ModelMapperV12 extends ModelMapper[v1_2.ExecutionPlan, v1_2.ExecutionEvent] {
-
-  import ImplicitMappingConversions._
 
   override def toDTO(plan: ExecutionPlan): Option[v1_2.ExecutionPlan] = Some(v1_2.ExecutionPlan(
     id = plan.id,
-    name = plan.name,
+    name = Some(plan.name),
     discriminator = plan.discriminator,
-    labels = plan.labels,
+    labels = plan.labels.asNonEmptyOption,
     operations = toOperations(plan.operations),
-    attributes = plan.attributes.map(automap(_).to[v1_2.Attribute]),
+    attributes = plan.attributes.map(toAttribute).asNonEmptyOption,
     expressions = Some(toExpressions(plan.expressions)),
-    systemInfo = automap(plan.systemInfo).to[v1_2.NameAndVersion],
-    agentInfo = Some(automap(plan.agentInfo).to[v1_2.NameAndVersion]),
-    extraInfo = plan.extraInfo
+    systemInfo = toNameAndVersion(plan.systemInfo),
+    agentInfo = Some(toNameAndVersion(plan.agentInfo)),
+    extraInfo = toUntypedMap(plan.extraInfo).asNonEmptyOption
   ))
 
   def toOperations(operations: Operations): v1_2.Operations = v1_2.Operations(
-    write = automap(operations.write).to[v1_2.WriteOperation],
-    reads = operations.reads.map(automap(_).to[v1_2.ReadOperation]),
-    other = operations.other.map(automap(_).to[v1_2.DataOperation])
+    write = toWriteOperation(operations.write),
+    reads = operations.reads.map(toReadOperation).asNonEmptyOption,
+    other = operations.other.map(toDataOperation).asNonEmptyOption
   )
 
-  def toExpressions(expressions: Expressions): v1_2.Expressions = v1_2.Expressions(
-    functions = expressions.functions.map(automap(_).to[v1_2.FunctionalExpression]),
-    constants = expressions.constants.map(automap(_).to[v1_2.Literal])
+  def toWriteOperation(operation: WriteOperation): v1_2.WriteOperation = v1_2.WriteOperation(
+    outputSource = operation.outputSource,
+    append = operation.append,
+    id = operation.id,
+    name = Some(operation.name),
+    childIds = operation.childIds,
+    params = toUntypedMap(operation.params).asNonEmptyOption,
+    extra = toUntypedMap(operation.extra).asNonEmptyOption
   )
 
-  override def toDTO(event: ExecutionEvent): Option[v1_2.ExecutionEvent] = Some(automap(event).to[v1_2.ExecutionEvent])
+  def toReadOperation(operation: ReadOperation): v1_2.ReadOperation = v1_2.ReadOperation(
+    inputSources = operation.inputSources,
+    id = operation.id,
+    name = Some(operation.name),
+    output = operation.output.asNonEmptyOption,
+    params = toUntypedMap(operation.params).asNonEmptyOption,
+    extra = toUntypedMap(operation.extra).asNonEmptyOption
+  )
 
-  implicit def toAttrOrExprRef(o: Seq[AttrOrExprRef]): Option[Seq[v1_2.AttrOrExprRef]] = o.map {
+  def toDataOperation(operation: DataOperation): v1_2.DataOperation = v1_2.DataOperation(
+    id = operation.id,
+    name = Some(operation.name),
+    childIds = operation.childIds.asNonEmptyOption,
+    output = operation.output.asNonEmptyOption,
+    params = toUntypedMap(operation.params).asNonEmptyOption,
+    extra = toUntypedMap(operation.extra).asNonEmptyOption
+  )
+
+  def toAttribute(attribute: Attribute): v1_2.Attribute = v1_2.Attribute(
+    id = attribute.id,
+    dataType = attribute.dataType,
+    childRefs = attribute.childRefs.map(toAttrOrExprRef).asNonEmptyOption,
+    extra = toUntypedMap(attribute.extra).asNonEmptyOption,
+    name = attribute.name
+  )
+
+  def toAttrOrExprRef(o: AttrOrExprRef): v1_2.AttrOrExprRef = o match {
     case AttrRef(attrId) => v1_2.AttrOrExprRef(Some(attrId), None)
     case ExprRef(exprId) => v1_2.AttrOrExprRef(None, Some(exprId))
   }
+
+  def toUntypedMap(map: Map[String, Any]): Map[String,Any] =
+    ModelMapper.toUntypedMap(toAttrOrExprRef, map)
+
+  def toExpressions(expressions: Expressions): v1_2.Expressions = v1_2.Expressions(
+    functions = expressions.functions.map(toFunctionalExpression).asNonEmptyOption,
+    constants = expressions.constants.map(toConstant).asNonEmptyOption
+  )
+
+  def toFunctionalExpression(fe: FunctionalExpression): v1_2.FunctionalExpression = v1_2.FunctionalExpression(
+    id = fe.id,
+    dataType = fe.dataType,
+    childRefs = fe.childRefs.map(toAttrOrExprRef).asNonEmptyOption,
+    extra = toUntypedMap(fe.extra).asNonEmptyOption,
+    name = fe.name,
+    params = toUntypedMap(fe.params).asNonEmptyOption
+  )
+
+  def toConstant(l: Literal): v1_2.Literal = v1_2.Literal(
+    id = l.id,
+    dataType = l.dataType,
+    extra = toUntypedMap(l.extra).asNonEmptyOption,
+    value = l.value
+  )
+
+  def toNameAndVersion(nav: NameAndVersion): v1_2.NameAndVersion = v1_2.NameAndVersion(
+    name = nav.name,
+    version = nav.version
+  )
+
+  override def toDTO(event: ExecutionEvent): Option[v1_2.ExecutionEvent] = Some(v1_2.ExecutionEvent(
+    planId = event.planId,
+    labels = event.labels.asNonEmptyOption,
+    timestamp = event.timestamp,
+    durationNs = event.durationNs,
+    discriminator = event.discriminator,
+    error = event.error,
+    extra = toUntypedMap(event.extra).asNonEmptyOption
+  ))
+
 }
