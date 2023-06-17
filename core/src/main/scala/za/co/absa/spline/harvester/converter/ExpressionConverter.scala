c67770c31504faf15045aba225d74c6e3a50f04f
uday1409 <43748110+uday1409@users.noreply.github.com>
2023-06-17 10:33:22 +0530

    adding comments referring the issue for better readability in future

--- core/src/main/scala/za/co/absa/spline/harvester/converter/ExpressionConverter.scala
+++ core/src/main/scala/za/co/absa/spline/harvester/converter/ExpressionConverter.scala
@@ -83,7 +83,7 @@ class ExpressionConverter(
     case e: sparkExprssions.ListQuery =>
       FunctionalExpression(
         id = idGen.nextId(),
-        dataType = convertDataTypeNonNullable(e),
+        dataType = convertDataTypeNonNullable(e), //refer issue #700 for details why the standard method is not used
         childRefs = Seq.empty,
         extra = createExtra(e, ExprV1.Types.GenericLeaf),
         name = e.prettyName,
