d5e37d42bd64c8083a561d5d3c7781e5cc2993bc
Neelab Chaudhuri <nchaudhuri@fanatics.com>
2023-07-11 00:22:11 -0600

    added license info

--- core/src/main/scala/za/co/absa/spline/harvester/dispatcher/SqsLineageDispatcher.scala
+++ core/src/main/scala/za/co/absa/spline/harvester/dispatcher/SqsLineageDispatcher.scala
@@ -1,20 +1,36 @@
-package za.co.absa.spline.harvester.dispatcher.sqsdispatcher
+/*
+ * Copyright 2019 ABSA Group Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package za.co.absa.spline.harvester.dispatcher
 
 import org.apache.commons.configuration.Configuration
 import org.apache.spark.internal.Logging
 import software.amazon.awssdk.services.sqs.SqsClient
 import software.amazon.awssdk.services.sqs.model.SendMessageRequest
 import za.co.absa.commons.version.Version
-import za.co.absa.spline.harvester.dispatcher.LineageDispatcher
 import za.co.absa.spline.harvester.dispatcher.modelmapper.ModelMapper
+import za.co.absa.spline.harvester.dispatcher.sqsdispatcher.SqsLineageDispatcherConfig
 import za.co.absa.spline.producer.model.{ExecutionEvent, ExecutionPlan}
 
-class SqsLineageDispatcherImpl(sqsClient: SqsClient,
-                               sqsUrl: String,
-                               apiVersion: Version) extends LineageDispatcher with Logging {
+class SqsLineageDispatcher(sqsClient: SqsClient,
+                           sqsUrl: String,
+                           apiVersion: Version) extends LineageDispatcher with Logging {
   import za.co.absa.spline.harvester.json.HarvesterJsonSerDe.impl._
   def this(dispatcherConfig: SqsLineageDispatcherConfig) = this(
-    SqsLineageDispatcherImpl.createSqsClient(dispatcherConfig),
+    SqsLineageDispatcher.createSqsClient(dispatcherConfig),
     dispatcherConfig.queueUrl,
     dispatcherConfig.apiVersion
   )
@@ -72,7 +88,7 @@ class SqsLineageDispatcherImpl(sqsClient: SqsClient,
 }
 
 
-object SqsLineageDispatcherImpl extends Logging {
+object SqsLineageDispatcher extends Logging {
 
   private def createSqsClient(config: SqsLineageDispatcherConfig): SqsClient = {
     SqsClient
