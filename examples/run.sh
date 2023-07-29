3847578b7d42b7cc553cb955cf13fccb45186675
Oleksandr Vayda <oleksandr.vayda@gmail.com>
2023-07-30 00:48:53 +0200

    issue #726 New scripts to run examples - greatly simplified, no Maven or Ant dependency.

--- examples/run.sh
+++ examples/run.sh
@@ -13,24 +13,39 @@ print_line() {
 }
 
 if [ $# -eq 0 ]; then
-  echo "Usage: $0 [--all | full.class.Name] [-jvm-option]..."
-  echo "  --all            run all examples."
-  echo "  full.class.Name  fully qualified class name to run."
-  echo "  -jvm-option      options and values passed to the java command."
-  exit 1
+    echo "Usage: $0 [--all [jvm_opts]... | [jvm_opts]... full.class.Name [prg-args]...]"
+    echo "  --all            run all examples."
+    echo "  full.class.Name  fully qualified class name to run."
+    echo "  -jvm-opts        JVM options."
+    echo "  -prg-args        program arguments."
+    exit 1
 fi
 
 trap ctrl_c SIGINT SIGTERM
 
-CLASSPATH=target/classes:$(echo target/libs/*.jar | tr ' ' ':')
+classpath=target/classes:$(echo target/libs/*.jar | tr ' ' ':')
 
 if [ "$1" = "--all" ]; then
-  while IFS= read -r -d '' file; do
-    class=$(echo "${file#target/classes/}" | sed 's/\//./g' | sed 's/\.class$//')
-    print_line "$class"
-    java "${@:2}" -cp "$CLASSPATH" "$class"
-  done < <(find target/classes -type f -name "*Job.class" -print0 | sort -z)
+    while IFS= read -r -d '' file; do
+        class=$(echo "${file#target/classes/}" | sed 's/\//./g' | sed 's/\.class$//')
+        print_line "$class"
+        java "${@:2}" -cp "$classpath" "$class"
+    done < <(find target/classes -type f -name "*Job.class" -print0 | sort -z)
 else
-  print_line "$1"
-  java "${@:2}" -cp "$CLASSPATH" "$1"
+    class_pos=1
+    for arg in "$@"
+    do
+        if [[ $arg != -* ]]
+        then
+            break
+        fi
+        class_pos=$((class_pos+1))
+    done
+
+    class=${!class_pos}
+    options=("${@:1:$class_pos-1}")
+    arguments=("${@:$class_pos+1}")
+
+    print_line "$class"
+    java "${options[@]}" -cp "$classpath" "$class" "${arguments[@]}"
 fi
